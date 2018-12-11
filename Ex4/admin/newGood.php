3d6a61d88802014f602a572093d60b0087457be9
Леонид <l.bratslavsky@neurobotics.ru>
2018-12-11 16:27:24 +0300

    HW6

--- Ex4/admin/newGood.php
+++ Ex4/admin/newGood.php
@@ -5,6 +5,6 @@
 
 	move_uploaded_file($_FILES["photo"]["tmp_name"], $path . $_FILES["photo"]["name"]);
 
-	$sql = "insert into goods(name, photo, info, description, price) values ('".$_POST['name']."', '".$_FILES["photo"]["name"]."', '".$_POST['short_desc']."', '".$_POST['desc']."', '".$_POST['price']."')";
+	$sql = "insert into goods(name, photo, info, description, price) values('".$_POST['name']."', '".$_FILES["photo"]["name"]."', '".$_POST['short_desc']."', '".$_POST['desc']."', '".$_POST['price']."')";
 
 	mysqli_query($connect, $sql);
